7baa19f10656547209de9708c9212ca9b44f9391
chenyongzhiaaron <262667641@qq.com>
2019-02-15 18:03:18 +0800

    update all

--- run_tests.py
+++ run_tests.py
@@ -11,16 +11,30 @@ sys.path.append('./db_fixture')
 sys.path.append('./Global_bse')
 
 # 指定测试用例为当前文件夹下的 interface 目录
-test_dir = 'F:\/auto\qsjInterface\interface'
+test_dir = '.\interface'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='*.py')
 
 if __name__ == "__main__":
     # test_data.init_data()  # 初始化接口测试数据
     now = time.strftime("%Y-%m-%d %H_%M_%S")
-    filename = "F:\/auto\qsjInterface\/report/" + now + '_result.html'
+    filename = "./report/" + now + '_result.html'
     fp = open(filename, 'wb')
     runner = HTMLTestRunner(stream=fp,
                             title='QSJ API Test Report',
                             description='Implementation Example with: QSJ_API_Test_Report')
+
+
+# 指定测试用例为当前文件夹下的 interface 目录
+# test_dir = 'F:\/auto\qsjInterface\interface'
+# discover = unittest.defaultTestLoader.discover(test_dir, pattern='*.py')
+#
+# if __name__ == "__main__":
+#     # test_data.init_data()  # 初始化接口测试数据
+#     now = time.strftime("%Y-%m-%d %H_%M_%S")
+#     filename = "F:\/auto\qsjInterface\/report/" + now + '_result.html'
+#     fp = open(filename, 'wb')
+#     runner = HTMLTestRunner(stream=fp,
+#                             title='QSJ API Test Report',
+#                             description='Implementation Example with: QSJ_API_Test_Report')
     runner.run(discover)
     fp.close()
